c72027b0f4094acd49299ddecc9d518f7fad1453
Nammon <nammol05@gmail.com>
2025-02-14 10:39:40 +0700

    first commit”

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/showname')
+  getName(): string {
+    return this.appService.getName();
+  }
+
+  @Get('/myJson')
+  myJSON() {
+    return this.appService.myJSON();
+  }
+
+  @Get('/showinfo')
+  getInfo() {
+    return this.appService.getInfo();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'We love NestJS!';
+  }
+
+  getName(): string {
+    return 'Nammon Koedbuathong';
+  }
+
+  myJSON() {
+    return {
+      name: 'Nammon',
+      lastname: 'Koedbuathong',
+      age: 19,
+    };
+  }
+
+  getInfo(): string {
+    return 'Hello, I am Nammon Koedbuathong, I\'m 19 years old'
+  }
+}
